3729499cc228900d46308d74d4d1d31769fca2da
knosence <nadario.seays@gmail.com>
2019-07-23 13:26:08 -0400

    first commit

---
+++ 1.Arrays/rust/1-get-product-of-all-other-elements.rs
@@ -0,0 +1,18 @@
+fn products(_arr: &mut [i32]) {
+    let vec_nums = vec![1,1,1,1,1];
+
+    for i in 0..(vec_nums.len()-1) {
+    }
+
+
+    for j in (vec_nums.len()-1)..0 {
+
+    }
+}
+
+fn main(){
+
+    let mut small_array: [i32; 5] = [1,2,3,4,5];
+    products(&mut small_array);
+    
+}
